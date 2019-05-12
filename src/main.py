e381e0bdc4905c7a02e988e736fcc2f09e515a11
Dess96 <dessalfaro@gmail.com>
2019-05-12 13:25:17 -0600

    Aun no esta terminado
    *Elegir orden segun color de fichas

--- src/main.py
+++ src/main.py
@@ -1,60 +1,81 @@
 from TableroGo import TableroGo  # Se importa la clase como tal y no como modulo
 from JugadorGo import JugadorGo
+import sys
 
 
 def main():
+    print("Bienvenido al juego de Go \n");
+    print("1. Jugar\n");
+    print("2. Salir del menu\n");
 
-    continuar = 1
-    proximo_en_jugar = 1
-    tableroGo = TableroGo()  # Instancia de la clase Tablero
-    tableroGo.crear_tablero()
-    tableroGo.mostrar_tablero()
-    print("Jugador  1  ha perdido:  0 piezas \n")
+    opcion = int(input("Digite una opcion: "))
+    if(opcion == 1):
+        continuar = 1
+        proximo_en_jugar = 1
+        tableroGo = TableroGo()  # Instancia de la clase Tablero
+        tableroGo.crear_tablero()
+       
+        print("Que color de ficha desea? \n")
+        print("1. Fichas Negras \n")
+        print("2. Fichas Blancas\n")
+        color = int(input("Digite una opcion: "))
+        if(color == 1):
+            print("El jugador 1 juega con las fichas negras")
+            jugador1 = JugadorGo(1)
+            jugador2 = JugadorGo(2)
+       
+        else:
+            print("El jugador 1 juega con las fichas blancas")
+            jugador1 = JugadorGo(2)
+            jugador2 = JugadorGo(1)
+        
+        print("Inicia el jugador con las fichas negras")
+        tableroGo.mostrar_tablero()
+     #   print("Jugador ", jugador.obt_id(), "ha perdido:  0 piezas \n")
+        #while(continuar == 1 ):
+        #    try:
+        #        if(proximo_en_jugar == 1):
+        #            print("Jugador 1 en turno")
+        #            fila = int(input("Ingrese la fila en la que desea colocar la ficha: "))
+        #            columna = int(input("Ingrese la columna en la que desea colocar la ficha: "))
+                    
+        #            if (jugador1.colocar_ficha(fila, columna, tableroGo) == False):
+        #                    print("No\n")
+        #            else: 
+        #                proximo_en_jugar = 2
+            
+        #        else:
+        #            print("Jugador 2 en turno")
+        #            fila = int(input("Ingrese la fila en la que desea colocar la ficha: "))
+        #            columna = int(input("Ingrese la columna en la que desea colocar la ficha: "))
 
-    jugador1 = JugadorGo(1)
-    #print(jugador1.obt_piezas_restantes())
-    # jugador1.eliminar_pieza()
-    # print(jugador1.obt_piezas_restantes())
-
-    jugador2 = JugadorGo(2)
-    #print(jugador1.obt_piezas_restantes())
-    # jugador1.eliminar_pieza()
-    # print(jugador1.obt_piezas_restantes())
+        #            if (jugador2.colocar_ficha(fila, columna, tableroGo) == False):
+        #                print("No\n")
+        #            else:    
+        #                proximo_en_jugar = 1
+                    
+        #        tableroGo.mostrar_tablero()
+            
+        #        if(proximo_en_jugar == 1):
+        #            jugador2.analizar_jugada(jugador1)
+        #        else:
+        #            jugador1.analizar_jugada(jugador2)    
+                    
+        #    except ValueError : 
+        #        print("Debe ingresar un valor de celda correcto")
+        #    except KeyboardInterrupt :
+        #        print("Se interrumpio el programa con Ctrl + C ")
     
-    while(continuar == 1 ):
+           
+    else:
         try:
-            if(proximo_en_jugar == 1):
-                print("Jugador 1 en turno")
-                fila = int(input("Ingrese la fila en la que desea colocar la ficha: "))
-                columna = int(input("Ingrese la columna en la que desea colocar la ficha: "))
-                    
-                if (jugador1.colocar_ficha(fila, columna, tableroGo) == False):
-                        print("No\n")
-                else: 
-                    proximo_en_jugar = 2
-            
-            else:
-                print("Jugador 2 en turno")
-                fila = int(input("Ingrese la fila en la que desea colocar la ficha: "))
-                columna = int(input("Ingrese la columna en la que desea colocar la ficha: "))
+            print("Presione cualquier tecla para salir")
+        except KeyboardInterrupt:
+            sys.exit()
 
-                if (jugador2.colocar_ficha(fila, columna, tableroGo) == False):
-                    print("No\n")
-                else:    
-                    proximo_en_jugar = 1
-                    
-            tableroGo.mostrar_tablero()
-            
-            if(proximo_en_jugar == 1):
-                jugador2.analizar_jugada(jugador1)
-            else:
-                jugador1.analizar_jugada(jugador2)    
-                    
-        except ValueError : 
-            print("Debe ingresar un valor de celda correcto")
-        except KeyboardInterrupt :
-            print("Se interrumpio el programa con Ctrl + C ")
-            return 0 
+
+    
+        return 0 
             
     
            
