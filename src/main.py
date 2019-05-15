9712a8d3198d5eafd34d1f0f0505df6111ac6fb4
Dess96 <dessalfaro@gmail.com>
2019-05-15 12:08:34 -0600

    bug: Corregido un error de parametros

--- src/main.py
+++ src/main.py
@@ -30,8 +30,8 @@ def main():
 
             else:
                 print("\n\nEl jugador 1 juega con las fichas blancas")
-                jugador1 = JugadorGo(1, blanco)
-                jugador2 = JugadorGo(2, negro)
+                jugador1 = JugadorGo(1, blanco, nombre1)
+                jugador2 = JugadorGo(2, negro, nombre2)
                 proximo_en_jugar = 2
 
             # Este print puede omitirse una vez que tengamos las reglas
